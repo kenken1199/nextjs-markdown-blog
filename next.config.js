e9040ece98040da1c5fd84c4b6da2a8a299b8911
kenken1199 <nakamori.k@gmail.com>
2023-02-19 16:13:15 +0900

    fix image domain

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["https://kenken1199.netlify.app/"],
-  },
 };
 
 module.exports = nextConfig;
